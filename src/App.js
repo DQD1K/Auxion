627e5465c7d8950629cddd986cddbb681aa9a6cd
DQD1K <dreadgenius110@gmail.com>
2024-05-18 16:27:43 +0300

    Попытка сделать json для тестирования

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import Navigation from "./components/navigation";
 import Header from "./components/components1,1/Header"
 import Account from "./components/account_page"
 import Auth from "./components/authtorization_page"
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({
+    name: 'name',
+    email: 'email@mail.com',
+    password: 'password'
+  })
+
+
   return (
     <div>
       <Header />
