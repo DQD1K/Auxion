8eab414f2d385d1abdd7038fdbfe124d06b9fed8
@kondrashov.dm <kondrashov.dm@nppgamma.ru>
2024-06-09 16:30:30 +0300

    Я сделать

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import './style.css'
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Mainpage from "./components/main_page";
-import Registr from "./components/registration_page";
-import Navigation from "./components/navigation";
 import Header from "./components/components1,1/Header"
-import Account from "./components/account_page"
-import Auth from "./components/authtorization_page"
-import { useState } from 'react';
-
 function App() {
-  const [data, setData] = useState({
-    name: 'name',
-    email: 'email@mail.com',
-    password: 'password'
-  })
-
 
   return (
     <div>
--- src/components/components1,1/Header.js
+++ src/components/components1,1/Header.js
@@ -1,15 +1,17 @@
-// import Recat from "react";
-import Logo from "./sticker.png";
-import ava from "./sticker (1).png"
-import BurgerMenu from './BurgerMenu'
-
 function Header(){
     return(
         <div>
-            <header>
-                <h1>Trade Fix</h1>
-            </header>
+            <h1>Trade Fix</h1>
+            <h2>Лучший сайт для аукциона</h2>
+
+            <h3>Ваши ставки</h3>
+            <h4>Вы же ещё не забыли про них, верно?</h4>
+
+            <h3 class="beit">Популярное за неделю</h3>
+            <h4 class="beit">Лучшие предложения на рынке</h4>
         </div>
+
+
     );
 }
 
