0fc57419573a4df84fb70fbe77d30496ff941209
@kondrashov.dm <kondrashov.dm@nppgamma.ru>
2024-06-09 17:03:34 +0300

    Ещё сделать

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './style.css'
-import React from 'react';
-import Header from "./components/components1,1/Header"
+import React, { useState } from 'react';
+import Header from "./components/components1,1/Header";
+import MyLTS from "./components/components1,1/my_lots";
+
 function App() {
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleMainClick = () => {
+    setShowHeader(true);
+  };
+
+  const handleMyLotsClick = () => {
+    setShowHeader(false);
+  };
 
   return (
     <div>
-      <Header />
-
+      <div>
+        <dir style={{ position: "absolute", left: "80px", top: "18px" }} className="maintext2" onClick={handleMainClick}>Главная</dir>
+        <dir style={{ position: "absolute", left: "200px", top: "18px" }} className="maintext2" onClick={handleMyLotsClick}>Мои лоты</dir>
+      </div>
+      
+      {showHeader ? <Header /> : <MyLTS />}
     </div>
-      );
+  )
 }
 
 export default App;
---
+++ src/components/components1,1/my_lots.js
@@ -0,0 +1,14 @@
+function my_lots(){
+    return(
+        <div>
+            <div class="ave_div">  
+
+
+            </div>
+        </div>
+
+
+    );
+}
+
+export default my_lots;
